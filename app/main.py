a1a53ecd0369e7135ba4b7c65c24794db7bf461c
vhsrover <egormetr@gmail.com>
2025-01-29 23:36:40 +0200

    car wash fixed

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class CarWashStation:
         for car in car_list:
             if car.clean_mark < self.clean_power:
                 station_income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+                self.wash_single_car(car)
         return round(station_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
